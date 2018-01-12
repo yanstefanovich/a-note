013864f6b89f4d41ca65e1a8b69abd491a5232d1
Yan <yan.stefanovich@ryerson.ca>
2018-01-11 23:58:43 -0500

    Modified Message

--- helpers/auth.js
+++ helpers/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     if (req.isAuthenticated()) {
       return next();
     } else {
-      req.flash('error_msg','You Must Be Logged In To Access That');
+      req.flash('error_msg','You Must Be Logged In To Access That Feature');
       res.redirect('/users/login');
     }
   }
